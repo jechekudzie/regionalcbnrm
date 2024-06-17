d4cb7ce30c2c27e52e16b921a50f37c75fbadae2
Nigel Jeche <jechekudzie@gmail.com>
2024-06-17 11:02:44 +0200

    Dashboards Update - v1.0.0 - Update

--- resources/views/reports/hunting_dashboard_district.blade.php
+++ resources/views/reports/hunting_dashboard_district.blade.php
@@ -73,14 +73,14 @@
                     datasets: [{
                         label: 'Allocated',
                         data: allocatedData,
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        backgroundColor: 'rgba(255, 99, 132, 0.6)',
                         borderColor: 'rgba(75, 192, 192, 1)',
                         borderWidth: 1
                     }, {
                         label: 'Utilised',
                         data: utilisedData,
-                        backgroundColor: 'rgba(255, 159, 64, 0.2)',
-                        borderColor: 'rgba(255, 159, 64, 1)',
+                        backgroundColor: 'rgba(153, 102, 255, 0.6)',
+                        borderColor: 'rgba(153, 102, 255, 1)',
                         borderWidth: 1
                     }]
                 },
